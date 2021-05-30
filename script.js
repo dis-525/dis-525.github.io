d99aa8b83a77acba2e4943e125efe17facf2020c
alp tugan <alptugan@gmail.com>
2021-05-30 16:41:42 +0300

    update

--- script.js
+++ script.js
@@ -3,10 +3,23 @@
 var xPos = 55;
 var yPos = 12;
 var w = 300;
-var h = 350;
+var h = 380;
+
+let totalContentH = 0;
+let thumb;
+let thumbSrc = "https://drive.google.com/uc?export=view&id=";
+let title = "Some Kind of Title";
+let author = "Name Surname";
+let desc = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
 let btnClose;
 let isShowContent;
+let _feature;
+
+
+// STORY DICT
+var cityDict;
+
 
 
 // LEAFLET STARTS
@@ -29,6 +42,20 @@ const markerOptions = {
     onEachFeature: onEachFeature
 };
 
+var info = L.control();
+info.onAdd = function(map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+info.update = function(props) {
+    this._div.innerHTML = '<h4>Artified Research Recordings</h4>' + (props ?
+        '<b>' + props.name + '</b><br />' + props.author :
+        'Hover over a city');
+};
+
+info.addTo(mymap);
 
 // BIND MOUSE OVER & OUT EVENTS
 function onEachFeature(feature, layer) {
@@ -36,6 +63,17 @@ function onEachFeature(feature, layer) {
         mouseover: highlightFeature,
         mouseout: resetHighlight,
         click: onClickCity
+            /*click: function(e) {
+                document.getElementById("info").innerHTML = feature.properties.name;
+                $("#feature_infos").stop();
+                $("#feature_infos").fadeIn("fast");
+
+                console.log(feature.properties.name);
+                //$("#feature_infos").fadeOut(5000);
+                // This is your click handler. 
+                // Your feature is available here as e.target, and the 
+                //featureInfo object we added is available as e.target.featureInfo 
+            }*/
     });
 }
 
@@ -47,6 +85,8 @@ function resetHighlight(e) {
         color: '#ff6b6b',
         fillOpacity: 0.5
     });
+
+    info.update();
 }
 
 // ON MOUSE OVER
@@ -63,15 +103,20 @@ function highlightFeature(e) {
     if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
         layer.bringToFront();
     }
+
+    info.update(layer.feature.properties);
 }
 
 function onClickCity(e) {
+
     isShowContent = true;
-    draw();
+    let thumbId = "1jOSKaYD5hWpgOMvBhejxewbsjwf7ORDv";
+    // Call related data
+    thumb = loadImage("test.jpg");
 }
 // DATABASE RELATED
 get_cities_from_drive((cities) => {
-    var cityDict = {};
+    cityDict = {};
     cities.forEach(city => {
         const domFeature = L.geoJSON(city.geojson, markerOptions)
             .addTo(mymap);
@@ -79,11 +124,12 @@ get_cities_from_drive((cities) => {
     });
 
     get_stories_from_drive((stories) => {
-        console.log(stories);
+        // console.log(stories);
         stories.forEach(story => {
             var domFeature = cityDict[story.city];
             if (domFeature) {
                 domFeature.bindPopup(story.soundcloud_iframe);
+
             } else {
                 console.error("city is not defined: " + story.city);
             }
@@ -116,13 +162,50 @@ function draw() {
     //
     if (isShowContent) {
 
-        fill(0, 0, 0, 100);
+        fill(0, 0, 0, 130);
         noStroke();
-        rect(xPos, yPos, w, h);
+        rect(xPos, yPos, w, h, 10);
 
         fill(255);
         textSize(12);
-        text("Click to Close Window", xPos + 10, yPos + 10, w - 40, h)
+        let curYPos = yPos + 10;
+        text("Click to Close Window", xPos + 10, curYPos, w - 40, h);
+
+        // Display thumbnail
+        curYPos += 20;
+        image(thumb, xPos + 10, curYPos);
+
+        // Display author name
+        curYPos += thumb.height + 8;
+        fill('#cccccc');
+        textStyle(BOLD);
+        text("Author", xPos + 10, curYPos, w - 40, h);
+        fill('#ffffff');
+        textStyle(NORMAL);
+
+        curYPos += 15;
+        text(author, xPos + 10, curYPos, w - 40, h);
+
+        // Display title
+        curYPos += 20;
+        fill('#cccccc');
+        textStyle(BOLD);
+        text("Title", xPos + 10, curYPos, w - 15, h);
+        fill('#ffffff');
+        textStyle(NORMAL);
+        curYPos += 15;
+        text(title, xPos + 10, curYPos, w - 10, h);
+
+
+        // Display description
+        curYPos += 20;
+        fill('#cccccc');
+        textStyle(BOLD);
+        text("Description", xPos + 10, curYPos, w - 15, h);
+        fill('#ffffff');
+        textStyle(NORMAL);
+        curYPos += 15;
+        text(desc, xPos + 10, curYPos, w - 10, h);
     }
     //console.log(mouseX);
 }
